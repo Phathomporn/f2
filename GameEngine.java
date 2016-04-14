12540569f5dd1fba40c4d9edc196e979c1e5d3da
Phathomporn <ohmplang20@gmail.com>
2016-04-14 20:59:59 +0700

    #03 ทำให้อุกาบาตตกลงมาได้แล้วค่ะ

--- GameEngine.java
+++ GameEngine.java
@@ -5,14 +5,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
+import java.util.Iterator;
 import java.util.ArrayList;
 import javax.swing.Timer;
 
 
 public class GameEngine{
 	GamePanel gp;
-	
+	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();	
 	
 	private Timer timer;
 	
@@ -37,13 +37,22 @@ public class GameEngine{
 	private void generateEnemy(){
 		Enemy e = new Enemy((int)(Math.random()*390), 30);
 		gp.sprites.add(e);
+		enemies.add(e);
 	}
 
 	private void process(){
 		gp.updateGameUI();
 		generateEnemy();
-		
-		
+
+		Iterator<Enemy> e_iter = enemies.iterator();
+		while(e_iter.hasNext()){
+			Enemy e = e_iter.next();
+			e.proceed();
+
+			if(!e.isAlive()){
+				e_iter.remove();
+			}
+		}
 	}
 	
 	public void die(){
