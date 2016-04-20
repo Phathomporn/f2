962102e24d3ccd98d3e979c17879b6e809123cfb
Phathomporn <ohmplang20@gmail.com>
2016-04-20 22:40:21 +0700

    #05 ให้จรวจเคลื่อนที่ได้แล้วค่ะ

--- GameEngine.java
+++ GameEngine.java
@@ -6,15 +6,17 @@ import java.awt.geom.Rectangle2D;
 import java.util.Iterator;
 import java.util.ArrayList;
 import javax.swing.Timer;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-
-public class GameEngine{
+public class GameEngine implements KeyListener{
 	GamePanel gp;
 
 	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();	
 	private SpaceShip v;
 	private Timer timer;
-	
+	private double difficulty = 0.1;
+
 	public GameEngine(GamePanel gp, SpaceShip v) {
 		this.gp = gp;
 		this.v = v;	
@@ -61,5 +63,34 @@ public class GameEngine{
 	
 	public void die(){
 		timer.stop();
+	}
+	void controlVehicle(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_LEFT:
+			v.move(-1);
+			break;
+		case KeyEvent.VK_RIGHT:
+			v.move(1);
+			break;
+		case KeyEvent.VK_D:
+			difficulty += 0.1;
+			break;
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		controlVehicle(e);
+		
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		//do nothing
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		//do nothing		
 	}
 }
